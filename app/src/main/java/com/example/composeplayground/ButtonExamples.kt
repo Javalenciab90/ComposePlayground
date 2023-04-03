dd9b48e8c2ba11bb4ee68a0b27eaaffbe2851bdd
Jaime Valencia <javalenciab90@gmail.com>
2023-04-02 19:25:33 -0500

    set button examples

--- app/src/main/java/com/example/composeplayground/ButtonExamples.kt
+++ app/src/main/java/com/example/composeplayground/ButtonExamples.kt
@@ -1,2 +1,47 @@
 package com.example.composeplayground
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true)
+@Composable
+fun  MyButtonExamples() {
+    var enabled by rememberSaveable { mutableStateOf(true) }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Button(
+            onClick = { enabled = false },
+            enabled = enabled,
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Magenta,
+                contentColor = Color.Blue
+            ),
+            border = BorderStroke(5.dp, Color.Green)
+        ) {
+            Text(text = "MyButton")
+        }
+
+        OutlinedButton(onClick = { }) {
+            Text(text = "MyButton")
+        }
+    }
+}
--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.internal.enableLiveLiterals
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,22 +39,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ButtonsPreview() {
     ComposePlaygroundTheme {
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(24.dp)
-        ) {
-            Button(
-                onClick = { Log.i("Aris", "Esto es un ejemplo") },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color.Magenta,
-                    contentColor = Color.Blue
-                ),
-                border = BorderStroke(5.dp, Color.Green)
-            ) {
-                Text(text = "MyButton")
-            }
-        }
+       MyButtonExamples()
     }
 }
 

24c1d52d9f82ab715a340f059da175adb0283139
Jaime Valencia <javalenciab90@gmail.com>
2023-04-02 19:09:47 -0500

    ButtonsExamples

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.composeplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,95 +18,55 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultPreview()
+            //TextFieldsPreview()
+            ButtonsPreview()
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ButtonsPreview() {
+    ComposePlaygroundTheme {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+        ) {
+            Button(
+                onClick = { Log.i("Aris", "Esto es un ejemplo") },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Magenta,
+                    contentColor = Color.Blue
+                ),
+                border = BorderStroke(5.dp, Color.Green)
+            ) {
+                Text(text = "MyButton")
+            }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun TextFieldsPreview() {
     ComposePlaygroundTheme {
         //ConstrainExample()
         //MyStateExample()
         //MyText()
-        //MyTextField()
         //MyTextFieldAdvance()
-        MyTextFieldOutLined()
+        //MyTextFieldOutLined()
+
+        var myText by remember { mutableStateOf("Aris")}
+        MyTextField(myText) { myText = it }
     }
 }
 
-@Composable
-fun MyTextFieldOutLined() {
-    var myText by remember { mutableStateOf("Aris")}
-    OutlinedTextField(
-        value = myText,
-        onValueChange = {myText = it},
-        modifier = Modifier.padding(24.dp),
-        label = { Text(text = "Holita")},
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = Color.Magenta,
-            unfocusedBorderColor = Color.Blue
-        )
-    )
-}
-
-@Composable
-fun MyTextFieldAdvance() {
-    var myText by remember { mutableStateOf("")}
-    TextField(
-        value = myText,
-        onValueChange = { myText = if (it.contains("a")) it.replace("a","") else it },
-        label = { Text(text = "Introduce tu nombre")}
-    )
-}
-
-@Composable
-fun MyTextField() {
-    var myText by remember { mutableStateOf("Aris")}
-    TextField(value = myText, onValueChange = {myText = it})
-}
-
-@Composable
-fun MyText() {
-    Column(Modifier.fillMaxSize()) {
-        Text(text = "Example 1")
-        Text(text = "Example 2", color = Color.Blue)
-        Text(text = "Example 3", fontWeight = FontWeight.ExtraBold)
-        Text(text = "Example 4", fontWeight = FontWeight.Light)
-        Text(text = "Example 5", style = TextStyle(fontFamily = FontFamily.Cursive))
-        Text(
-            text = "Example 6",
-            style = TextStyle(textDecoration = TextDecoration.LineThrough)
-        )
-        Text(
-            text = "Example 7",
-            style = TextStyle(textDecoration = TextDecoration.Underline)
-        )
-        Text(
-            text = "Example 8",
-            style = TextStyle(
-                textDecoration = TextDecoration.combine(
-                    listOf(
-                        TextDecoration.Underline,
-                        TextDecoration.LineThrough
-                    )
-                )
-            )
-        )
-        Text(text = "hola", fontSize = 25.sp)
-    }
-}
---
+++ app/src/main/java/com/example/composeplayground/TextFieldExamples.kt
@@ -0,0 +1,87 @@
+package com.example.composeplayground
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun MyTextFieldOutLined() {
+    var myText by remember { mutableStateOf("Aris") }
+    OutlinedTextField(
+        value = myText,
+        onValueChange = {myText = it},
+        modifier = Modifier.padding(24.dp),
+        label = { Text(text = "Holita") },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+    )
+}
+
+@Composable
+fun MyTextFieldAdvance() {
+    var myText by remember { mutableStateOf("") }
+    TextField(
+        value = myText,
+        onValueChange = { myText = if (it.contains("a")) it.replace("a","") else it },
+        label = { Text(text = "Introduce tu nombre") }
+    )
+}
+
+@Composable
+fun MyTextField(text: String, onValueChanged: (String) -> Unit) {
+    TextField(
+        value = text,
+        onValueChange = { onValueChanged(it) }
+    )
+}
+
+@Composable
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Example 1")
+        Text(text = "Example 2", color = Color.Blue)
+        Text(text = "Example 3", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Example 4", fontWeight = FontWeight.Light)
+        Text(text = "Example 5", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(
+            text = "Example 6",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "Example 7",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "Example 8",
+            style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(
+                        TextDecoration.Underline,
+                        TextDecoration.LineThrough
+                    )
+                )
+            )
+        )
+        Text(text = "hola", fontSize = 25.sp)
+    }
+}

86969595003add7f2871493532f2c4d9fafffc8b
Jaime Valencia <javalenciab90@gmail.com>
2023-04-02 19:45:57 -0500

    IconExample

---
+++ app/src/main/java/com/example/composeplayground/IconsExamples.kt
@@ -0,0 +1,16 @@
+package com.example.composeplayground
+
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun MyIcon() {
+    Icon(
+        imageVector = Icons.Rounded.Star,
+        contentDescription = "Icon"
+    )
+}
